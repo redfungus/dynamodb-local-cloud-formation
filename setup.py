782d03fcf34e63ecbe7405f8c77da2c723ccea65
mwas <francismwangi152@gmail.com>
2018-11-25 12:11:26 +0300

    adding setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dynamodb-local-cloud-formation',
+    version='0.1',
+    packages=['', 'test', 'dynamodb_cloud_formation'],
+    url='https://github.com/steven-bruce-au/dynamodb-local-cloud-formation',
+    license='MIT',
+    author='Steven Bruce',
+    author_email='',
+    description='Parser to allow Cloud Formation templates to be used with DynamoDB local'
+)

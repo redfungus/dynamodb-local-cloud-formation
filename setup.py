4c82db53f07a43b0490d680a1d5fb53d24397c99
mwas <francismwangi152@gmail.com>
2018-11-25 12:36:01 +0300

    adding entrypoint

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-
+from setuptools import setup
 setup(
     name='dynamodb-local-cloud-formation',
     version='0.1',
@@ -7,6 +6,11 @@ setup(
     url='https://github.com/steven-bruce-au/dynamodb-local-cloud-formation',
     license='MIT',
     author='Steven Bruce',
+    entry_points={
+        'console_scripts': [
+            "dynamodb_cloud_formation_cli = dynamodb_cloud_formation.parser:main"
+        ]
+    },
     author_email='',
     description='Parser to allow Cloud Formation templates to be used with DynamoDB local'
 )
